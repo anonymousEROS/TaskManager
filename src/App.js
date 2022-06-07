3f92dc78ffb1d92d6a77ada3f2ca148ed4f16ec4
anonymousEROS <dovala69@gmail.com>
2022-06-07 14:42:31 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+
+function App() {
+  const [showAddTask, setShowAddTask]  = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1, 
+        text: 'Doctors Appointment',
+        day: 'Aug 12th at 2:00pm',
+        reminder: true,
+    },
+    {
+        id: 2, 
+        text: 'Meeting at school',
+        day: 'Aug 13th at 2:00pm',
+        reminder: true,
+    },
+    {
+        id: 3, 
+        text: 'Food Shopping',
+        day: 'Aug 14th at 10:00am',
+        reminder: false,
+    },
+])
+
+//Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete Task 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ))
+  }
+
+  return (
+    <div className='container'>
+     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+     {showAddTask && <AddTask onAdd={addTask}/>}
+     {tasks.length > 0 ?<Tasks tasks ={tasks} onDelete= {deleteTask}
+     onToggle={toggleReminder}/> 
+     : ( ' No Tasks To Show')}
+      
+    </div>
+  )
+}
+
+export default App;
